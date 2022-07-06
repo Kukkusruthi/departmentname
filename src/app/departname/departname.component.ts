58af6e8170e7d9a11f6f2a59ba3f4136d1eeef8b
kukkusruthi <nsruthi054@gmail.com>
2022-07-06 21:55:12 +0530

    created

---
+++ src/app/departname/departname.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-departname',
+  templateUrl: './departname.component.html',
+  styleUrls: ['./departname.component.css']
+})
+export class DepartnameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
